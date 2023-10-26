4b642406de0a65eb571c9ef8ace792cf478e1075
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2023-10-26 16:18:00 +0200

    admin view, and superuser v0.01

--- surferhub/admin.py
+++ surferhub/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+# Importing models
+from .models import CustomUser, UserProfile
+
+
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    search_fields = ('username', 'email',)
+    list_filter = ('is_staff', 'is_active',)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country')
+    search_fields = ('user__username', 'country')
+    list_filter = ('country',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
